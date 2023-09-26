25ef4f1b6b04cf5797c934029e04c94b7320d3af
Ilia Salaur <salaur.ilie@gmail.com>
2023-09-27 00:47:53 +0200

    web1 removed

--- web1/src/DevicesContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from 'react';
-// export const DevicesContext = createContext({selectedDevice:0, devices:[{id:123,h:16,w:16,tag:'hi', ip:'192.168.0.2'}]});
-export const DevicesContext = createContext({selectedDevice:null, devices:[]});
